4720ee52f2d74b07b89be00d077d62dc78b51639
Christian M <seboss123@gmail.com>
2020-04-11 11:45:11 -0600

    Add widgets to color change

--- app/src/main/java/com/cmechem/grabcolorapp/FirstFragment.kt
+++ app/src/main/java/com/cmechem/grabcolorapp/FirstFragment.kt
@@ -1,29 +1,40 @@
 package com.cmechem.grabcolorapp
 
-import android.app.Activity
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import androidx.navigation.fragment.findNavController
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_first.*
-import java.lang.Exception
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+    private var viewToChangeColor: View? = null
+    var enableChangeButton = false
+
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
@@ -31,47 +42,171 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewToChangeColor = view.findViewById(R.id.showColor2)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        view.findViewById<TextView>(R.id.colorText).setOnClickListener {
+            viewToChangeColor = it
+            displayColorChangeAlert()
+
         }
-        view.findViewById<Button>(R.id.grabColorButton).setOnClickListener{
-           val intent = Intent(Intent.ACTION_PICK)
-            val chooser :Intent = Intent.createChooser(intent,"Please Choose a Color Picker")
-            startActivityForResult(chooser,1)
+
+
+        view.findViewById<Button>(R.id.grabColorButton).setOnClickListener {
+            if (enableChangeButton) {
+                viewToChangeColor = it
+            }
+            grabColor()
+        }
+        view.findViewById<Button>(R.id.button2).setOnClickListener {
+            if (enableChangeButton) {
+                viewToChangeColor = it
+            }
+            grabColor()
+        }
+
+        view.findViewById<View>(R.id.showColor).setOnClickListener {
+            viewToChangeColor = it
+            Log.e("You selected", viewToChangeColor.toString())
+            displayColorChangeAlert()
+        }
+        view.findViewById<View>(R.id.showColor2).setOnClickListener {
+            viewToChangeColor = it
+            displayColorChangeAlert()
+        }
+        changeButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                enableChangeButton = true
+            } else {
+                enableChangeButton = false
+                viewToChangeColor = null
+            }
+            Log.e("State is", enableChangeButton.toString())
+
         }
     }
+
+    //Open Colorpicking app
+    fun grabColor() {
+        val intent = Intent(Intent.ACTION_SEND)
+        val chooser: Intent = Intent.createChooser(intent, "Please Choose a Color Picker")
+        startActivityForResult(chooser, 1)
+    }
+
+    //Dialog to ask if you would like to change color of View
+    fun displayColorChangeAlert() {
+        val builder: AlertDialog.Builder? = activity?.let {
+            AlertDialog.Builder(it)
+        }
+        builder?.setMessage("Would you like to change the color?")
+            ?.setPositiveButton("Yes", { dialog, id ->
+                Log.e("This Worked", "LOLDASDA")
+                grabColor()
+            })
+            ?.setNegativeButton("No", { dialog, id ->
+            })
+        val dialog: AlertDialog? = builder?.create()
+        dialog?.show()
+    }
+
+    @SuppressLint("ObjectAnimatorBinding")
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-       try {
-           super.onActivityResult(requestCode, resultCode, data)
-           if (resultCode==android.app.Activity.RESULT_OK){
-               val anim = AlphaAnimation(1.0f, 0.0f)
-               val color = data?.getStringExtra("Color")
-               if (color!=null) Log.e("Color is:",color)
+        try {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (resultCode == android.app.Activity.RESULT_OK) {
+                val extras: Bundle? = data?.extras
+                val anim = AlphaAnimation(1.0f, 0.0f)
+                val viewColor1 = showColor.background as ColorDrawable
+                val oldColor1: Int = viewColor1.color
+                val viewColor2 = showColor2.background as ColorDrawable
+                val oldColor2: Int = viewColor2.color
+                Log.e("Color is", oldColor1.toString())
 
-               anim.duration = 2000
-               anim.repeatCount = 1
-               anim.repeatMode = Animation.REVERSE
-
-               anim.setAnimationListener(object : Animation.AnimationListener {
-                   override fun onAnimationEnd(animation: Animation?) { }
-                   override fun onAnimationStart(animation: Animation?) { }
-                   override fun onAnimationRepeat(animation: Animation?) {
-                       textview_first.text=color
-
-                   }
-               })
-
-               textview_first.startAnimation(anim)
-
-           }
+                var red: Int? = extras?.getInt("Red")
+                var green: Int? = extras?.getInt("Green")
+                var blue: Int? = extras?.getInt("Blue")
+                var backgroundColorChanger: ObjectAnimator
 
 
-       }
-       catch (e:Exception){
-           Toast.makeText(
-               context, e.toString(),
-               Toast.LENGTH_SHORT).show();
-       }
+                if (red != null && green != null && blue != null) {
+                    if (viewToChangeColor == view?.findViewById(
+                            R.id.showColor
+                        )
+                    ) {
+                        textView1.text = ""
+                        Log.e("Color is:", red.toString())
+                        backgroundColorChanger = ObjectAnimator.ofObject(
+                            viewToChangeColor, "backgroundColor", ArgbEvaluator(),
+                            oldColor1, Color.rgb(red, green, blue)
+                        )
+                        backgroundColorChanger.duration = 1000
+                        backgroundColorChanger.start()
+                    }
+                    //Change color of View2
+                    else if (viewToChangeColor == view?.findViewById(
+                            R.id.showColor2
+                        )
+                    ) {
+                        textView2.text = ""
+                        Log.e("Color is:", red.toString())
+                        backgroundColorChanger = ObjectAnimator.ofObject(
+                            viewToChangeColor, "backgroundColor", ArgbEvaluator(),
+                            oldColor2, Color.rgb(red, green, blue)
+                        )
+                        backgroundColorChanger.duration = 1000
+                        backgroundColorChanger.start()
+                    }
+                    //Different method to animate the color change of buttons
+                    else if (viewToChangeColor is Button) {
+                        Log.e("Color is:", red.toString())
+                        var rippleColor: Int = 0
+
+                        val rippleDrawable = viewToChangeColor!!.background
+                        val state: Drawable.ConstantState = rippleDrawable.constantState!!
+                        try {
+                            val colorField = state.javaClass.getDeclaredField("color")
+                            colorField.isAccessible = true
+                            val colorStateList = colorField.get(state) as ColorStateList
+                            rippleColor = colorStateList.defaultColor
+                        } catch (e: NoSuchFieldException) {
+                            e.printStackTrace()
+                        } catch (e: IllegalAccessException) {
+                            e.printStackTrace()
+                        }
+
+                        backgroundColorChanger = ObjectAnimator.ofObject(
+                            viewToChangeColor, "backgroundColor", ArgbEvaluator(),
+                            rippleColor, Color.rgb(red, green, blue)
+                        )
+                        backgroundColorChanger.duration = 1000
+                        backgroundColorChanger.start()
+                    }
+                    else if (viewToChangeColor is TextView){
+                        backgroundColorChanger = ObjectAnimator.ofObject(
+                            viewToChangeColor, "textColor",ArgbEvaluator(),oldColor1, Color.rgb(red, green, blue)
+
+                        )
+
+                        backgroundColorChanger.duration = 1000
+                        backgroundColorChanger.start()
+                    }
+                }
+            }
+
+
+        } catch (e: NullPointerException) {
+            Log.e("ERROr", e.toString())
+            Toast.makeText(
+                context, "Please select a widget to change color",
+                Toast.LENGTH_LONG
+            ).show()
+        } catch (e: Exception) {
+            Log.e("ERROr", e.toString())
+            Toast.makeText(
+                context, e.toString(),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
+
 }
--- app/src/main/java/com/cmechem/grabcolorapp/MainActivity.kt
+++ app/src/main/java/com/cmechem/grabcolorapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.cmechem.grabcolorapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
@@ -10,16 +11,20 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val SPLASH_TIME_OUT = 4000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        Handler().postDelayed(Runnable{
+            @Override
+            fun run(){
+                var homeIntent:Intent= Intent(MainActivity.this, HomeActivity.class)
+            }
+        })
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
     }
 
 
